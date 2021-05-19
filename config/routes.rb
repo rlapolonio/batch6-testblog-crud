e82e488fa11f7b99bf498866274cfdcf13b897f5
Rhenfel Apolonio <rlapolonio@gmail.com>
2021-05-19 22:02:09 +0800

    first commit

---
+++ config/routes.rb
@@ -0,0 +1,11 @@
+Rails.application.routes.draw do
+  root 'articles#index'
+
+  get '/articles' => 'articles#index', as: 'articles'
+  get '/articles/new' => "articles#new", as: 'new_article'
+  get '/articles/:id' => "articles#show", as: 'article'
+  post '/articles' => 'articles#create'
+  get '/articles/:id/edit' => 'articles#edit', as: 'edit_article' 
+  patch '/articles/:id' => 'articles#update'
+  delete '/articles/:id' => 'articles#destroy'
+end
---
+++ db/seeds.rb
@@ -0,0 +1,12 @@
+# This file should contain all the record creation needed to seed the database with its default values.
+# The data can then be loaded with the bin/rails db:seed command (or created alongside the database with db:setup).
+#
+# Examples:
+#
+#   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
+#   Character.create(name: 'Luke', movie: movies.first)
+
+Article.create([{name: 'Sample Name 1', body: 'Sample Body 1'},
+                {name: 'Sample Name 2', body: 'Sample Body 2'},
+                {name: 'Sample Name 3', body: 'Sample Body 3'},
+                {name: 'Sample Name 4', body: 'Sample Body 4'}])
